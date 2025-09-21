9047cf2b197ec3a80a730beec494a1ff2494af84
Faith Gbadebo <gbadebfaith@gmail.com>
2025-09-21 13:29:06 +0100

    added cloth recommendation

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,7 +4,7 @@ weather = input("What's the weather like today? (sunny/rainy/cold): ")
 if weather == "sunny":
    print("Wear a t-shirt and sunglasses.") 
 elif weather == "cold":
-    print("Make sure to wear a coat and a scarf.")
+    print("Make sure to wear a warm coat and a scarf.")
 elif weather == "rainy":
     print("Don't forget your umbrella and raincoat.")
 else:
