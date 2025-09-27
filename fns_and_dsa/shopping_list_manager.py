0f02fa0325d1c0abd6bbdc9820e3c8bd69e88891
Faith Gbadebo <gbadefaith25@gmail.com>
2025-09-27 22:33:13 +0100

    arithmetic operations

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,3 +1,20 @@
+def perform_operation(num1, num2, operation):
+    print("Arithmetic Operations")
+
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        return num1 / num2
+    else:
+        return "Enter the correct operation"
+
+
 # Example usage:
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
